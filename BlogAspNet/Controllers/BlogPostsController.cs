738c70c2b5e00e78165df67a15fef828a39a7281
Paweł Trela <paweltrelapro@gmail.com>
2024-07-02 22:33:23 +0100

    edit blog

--- BlogAspNet/Controllers/BlogPostsController.cs
+++ BlogAspNet/Controllers/BlogPostsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using BlogAspNet.Services;
+using BlogAspNet.Models.Entities;
 
 namespace BlogAspNet.Controllers;
 
@@ -28,5 +29,39 @@ public class BlogPostsController : Controller
         ViewBag.CategoryName = categoryName ?? "Brak kategorii";
         
         return View(blogPost);
+    }
+    
+    public IActionResult Edit(Guid id)
+    {
+        var blogPosts = BlogPostService.blogPosts;
+        var blogPost = blogPosts.FirstOrDefault(x => x.Id == id);
+        
+        if (blogPost == null)
+        {
+            return NotFound();
+        }
+
+        return View(blogPost);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit(BlogPost blogPost)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(blogPost);
+        }
+
+        var existingBlogPost = BlogPostService.blogPosts.FirstOrDefault(x => x.Id == blogPost.Id);
+        if (existingBlogPost == null)
+        {
+            return NotFound();
+        }
+
+        existingBlogPost.Title = blogPost.Title;
+        existingBlogPost.Content = blogPost.Content;
+        existingBlogPost.CategoryId = blogPost.CategoryId;
+
+        return RedirectToAction("Details", new { id = blogPost.Id });
     }
 }
--- BlogAspNet/obj/Debug/net8.0/BlogAspNet.AssemblyInfo.cs
+++ BlogAspNet/obj/Debug/net8.0/BlogAspNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogAspNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b335f54fb077a4319e6fd27b817076e756e16b12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4cc10037522ac54180f3d5459024c368b06fdb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogAspNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogAspNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
